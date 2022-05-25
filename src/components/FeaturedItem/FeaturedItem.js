1e7ab8684c34ff8a61249851b00abc29d3b82039
yuriipereverziev <yuripereverzev@gmail.com>
2022-05-25 18:50:19 +0300

    add drag & active

--- src/components/FeaturedItem/FeaturedItem.js
+++ src/components/FeaturedItem/FeaturedItem.js
@@ -1,33 +1,84 @@
-import React from "react";
+import React, {Component} from "react";
 import star from "../../assets/img/star.svg";
 
-const FeaturedItem = ({srcImage, title, price}) => {
+export default class FeaturedItem extends Component {
 
-    return (
-        <li className="featured-item">
-            <div className="featured-item__top">
-                <img className="featured-item__img" src={srcImage} alt={title}/>
-            </div>
-            <div className="featured-item__main">
+    state = {
+        isActive: false,
+        step: -1,
+    }
 
+    toggleClass = () => {
+        const currentState = this.state.isActive;
+        this.setState({
+            isActive: !currentState,
+        });
+    };
 
-                <span className="featured-item__price">
-                    ${price.newPrice} - ${price.oldPrice}
-                </span>
+    componentDidMount() {
+        document.addEventListener("keydown", this.handleKeyPress)
+    }
 
-                <h3 className="featured-item__title">{title}</h3>
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKeyPress)
+    }
 
-                <div className="featured-item__rating">
-                    <img src={star} alt="star"/>
-                    <img src={star} alt="star"/>
-                    <img src={star} alt="star"/>
-                    <img src={star} alt="star"/>
-                    <img src={star} alt="star"/>
+    handleKeyPress = (event) => {
+        if (event.keyCode === 39 && this.state.step < 5) {
+            this.setState(prevState => ({
+                step: prevState.step + 1
+            }))
+        } else if (event.keyCode === 37 && this.state.step > 0 ) {
+            this.setState(prevState => ({
+                step: prevState.step - 1
+            }))
+        }
+        else if (event.keyCode === 27 && this.state.step >= 0 ) {
+            this.setState( () => ({
+                step: -1
+            }))
+        }
+    }
+
+    render() {
+        const {step, isActive} = this.state
+        const {srcImage, title, price, index, onDragStart, onDrop, id } = this.props
+
+        const activeClass = `${isActive ? ' featured-item__active' : '' || step === index ? ' featured-item__active' : ''}`;
+
+        return (
+            <li className={`featured-item${activeClass}`}
+                id={id}
+                onClick={this.toggleClass}
+                onKeyDown={this.handleKeyPress}
+                draggable={true}
+                onDragOver={(e) => e.preventDefault()}
+                onDragStart={onDragStart}
+                onDrop={onDrop}>
+
+                <div className="featured-item__top">
+                    <img className="featured-item__img" src={srcImage} alt={title}
+                         onLoad={() => console.log("loaded")}
+                         onError={() => console.log("error")}/>
                 </div>
-                <a className="featured-item__btn btn" href="/#">SHOP NOW</a>
-            </div>
-        </li>
-    )
-}
+                <div className="featured-item__main">
 
-export default FeaturedItem
+                    <span className="featured-item__price">
+                        ${price.newPrice} - ${price.oldPrice}
+                    </span>
+
+                    <h3 className="featured-item__title">{title}</h3>
+
+                    <div className="featured-item__rating">
+                        <img src={star} alt="star"/>
+                        <img src={star} alt="star"/>
+                        <img src={star} alt="star"/>
+                        <img src={star} alt="star"/>
+                        <img src={star} alt="star"/>
+                    </div>
+                    <a className="featured-item__btn btn" href="/#">SHOP NOW</a>
+                </div>
+            </li>
+        )
+    }
+}
