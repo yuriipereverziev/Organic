ceb0954a463122d181d3f04733ac518e86153cb2
yuriipereverziev <yuripereverzev@gmail.com>
2022-06-30 10:58:56 +0300

    add: useState

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -1,143 +1,108 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './scss/Featured.scss';
 import PropTypes from 'prop-types';
 import FeaturedView from './FeaturedView';
 import StoreApi from '../../services/StoreApi';
 import withData from '../../hocs/withData';
 
-class Featured extends Component {
-  maxId = 100;
+let maxId = 100;
+let maxOrder = 100;
 
-  maxOrder = 100;
+const Featured = ({ data }) => {
+  const [dataCart, setDataCart] = useState(data);
+  const [dragId, setDragId] = useState(null);
 
-  constructor(props) {
-    super(props);
-    const { data } = this.props;
-    this.state = {
-      data,
-      dragId: null,
-    };
-  }
+  const sortItem = () => {
+    const newArray = [...dataCart];
 
-  sortItem = () => {
-    this.setState(({ data }) => {
-      const newArray = [...data];
-
-      newArray.sort((a, b) => {
-        return a.price > b.price ? 1 : -1;
-      });
-
-      return {
-        data: newArray
-      };
+    newArray.sort((a, b) => {
+      return a.price > b.price ? 1 : -1;
     });
+    setDataCart(newArray);
   };
 
-  bblSort = () => {
-    this.setState(({ data }) => {
-      const arr = [...data];
+  const bblSort = () => {
+    const arr = [...dataCart];
 
-      for (let i = 0; i < arr.length; i += 1) {
-        for (let j = 0; j < (arr.length - i - 1); j += 1) {
-          if (arr[j].price > arr[j + 1].price) {
-            const temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-          }
+    for (let i = 0; i < arr.length; i += 1) {
+      for (let j = 0; j < (arr.length - i - 1); j += 1) {
+        if (arr[j].price > arr[j + 1].price) {
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
         }
       }
+    }
 
-      return {
-        data: arr
-      };
-    });
+    setDataCart(arr);
   };
 
-  deleteItem = () => {
-    this.setState(({ data }) => {
-      const newArray = [...data.slice(0, data.length - 1)];
-      return { data: newArray };
-    });
+  const deleteItem = () => {
+    setDataCart((s) => [...s.slice(0, dataCart.length - 1)]);
   };
 
-  addItem = () => {
-    this.maxId += 1;
-    this.maxOrder += 1;
+  const addItem = () => {
+    maxId += 1;
+    maxOrder += 1;
 
     const newItem = {
-      id: this.maxId,
-      order: this.maxOrder,
-      title: 'test product',
+      id: maxId,
+      order: maxOrder,
+      title: 'Root product',
       price: 13.5,
       description: 'lorem ipsum set',
       image: 'https://i.pravatar.cc',
       category: 'electronic'
     };
 
-    this.setState(({ data }) => {
-      const newArray = [...data, newItem];
-      return { data: newArray };
-    });
+    setDataCart((s) => [...s, newItem]);
   };
 
-  handleDrag = (e, id) => {
-    this.setState({
-      dragId: id
-    });
+  const handleDrag = (e, id) => {
+    setDragId(id);
   };
 
-  handleDrop = (e, card) => {
-    const { dragId } = this.state;
+  const handleDrop = (e, card) => {
+    const dragItem = dataCart.find((item) => item.id === dragId);
+    const dropItem = dataCart.find((item) => item.id === card.id);
 
-    this.setState(({ data }) => {
-      const newArray = [...data];
-
-      const dragItem = newArray.find((item) => item.id === dragId);
-      const dropItem = newArray.find((item) => item.id === card.id);
-
-      const newArrayData = newArray.map((item) => {
-        if (item.id === dragId) {
-          return {
-            ...item,
-            order: dropItem.order
-          };
-        }
-        if (item.id === card.id) {
-          return {
-            ...item,
-            order: dragItem.order
-          };
-        }
-        return item;
-      });
-
-      newArrayData.sort((a, b) => {
-        return a.order - b.order;
-      });
-
-      return { data: newArrayData };
+    const newData = dataCart.map((item) => {
+      if (item.id === dragId) {
+        return {
+          ...item,
+          order: dropItem.order
+        };
+      }
+      if (item.id === card.id) {
+        return {
+          ...item,
+          order: dragItem.order
+        };
+      }
+      return item;
     });
+
+    newData.sort((a, b) => {
+      return a.order - b.order;
+    });
+
+    setDataCart(newData);
   };
 
-  render() {
-    const { data } = this.state;
-
-    return (
-      <FeaturedView
-        featuredListData={data}
-        bblSort={this.bblSort}
-        sortItem={this.sortItem}
-        deleteItem={this.deleteItem}
-        addItem={this.addItem}
-        handleDrag={this.handleDrag}
-        handleDrop={this.handleDrop}
-      />
-    );
-  }
-}
+  return (
+    <FeaturedView
+      featuredListData={dataCart}
+      bblSort={bblSort}
+      sortItem={sortItem}
+      deleteItem={deleteItem}
+      addItem={addItem}
+      handleDrag={handleDrag}
+      handleDrop={handleDrop}
+    />
+  );
+};
 
 const { getProductsLimit } = new StoreApi();
-
 export default withData(Featured, getProductsLimit);
 
 Featured.propTypes = {
