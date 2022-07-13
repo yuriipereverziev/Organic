f8c08f7cc66ea8f0930f3ae0cc22de36be8b685a
yuriipereverziev <yuripereverzev@gmail.com>
2022-07-13 15:25:31 +0300

    add: few translates

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -15,5 +15,22 @@ export default {
     name: 'ORGANIC',
     btnText: 'SHOP NOW',
     banner: 'ORANGE LEMON'
-  }
+  },
+
+  discount: {
+    title: 'Get 10% off on Vegetables',
+    text: ' Shop our selection of organic fresh vegetables in a discounted price. 10% off on all vegetables.',
+    btn: 'SHOP NOW',
+  },
+
+  benefit: {
+    title: 'WE ARE HEALTHY FOOD ORGANIC',
+    // eslint-disable-next-line max-len
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+    bioTitle: 'Biological Benefits',
+    bioText: 'We\'re making room for self care today with plan.',
+    specTitle: 'Speciality Product',
+    specText: 'We\'re making room for self care today with plan.',
+    banner: 'ABOUT US',
+  },
 };
