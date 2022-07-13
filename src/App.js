3a5aa88b907c74e3bdf4000fcad4b1022186171d
yuriipereverziev <yuripereverzev@gmail.com>
2022-07-13 15:25:55 +0300

    add: provider redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Suspense } from 'react';
 import './App.scss';
+import { Provider } from 'react-redux';
 import ErrorBoundary from './components/ErrorBoundry/ErrorBoundary';
 import { ThemeProvider } from './context/ThemeDark/ThemeContext';
 import Root from './components/Root/Root';
 import './i18n';
 
+import store from './store/store';
+
 const App = () => {
   return (
-    <ErrorBoundary>
-      <ThemeProvider>
-        <Suspense fallback="Loading...">
-
-          <Root />
-        </Suspense>
-      </ThemeProvider>
-    </ErrorBoundary>
+    <Provider store={store}>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <Suspense fallback="Loading...">
+            <Root />
+          </Suspense>
+        </ThemeProvider>
+      </ErrorBoundary>
+    </Provider>
   );
 };
 export default App;
